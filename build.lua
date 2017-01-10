dd10c92e7f6043d6920dd31ee5b14d229c1bdef1
Ben Crist <ben@magicmoremagic.com>
2017-01-10 16:38:15 -0600

    link_project deps

--- build.lua
+++ build.lua
@@ -1,14 +1,13 @@
 module 'ctable' {
    lib {
       pch_src 'src/pch.cpp',
-      define 'BE_CTABLE_IMPL'
+      define 'BE_CTABLE_IMPL',
+      link_project {
+         'core', 'util'
+      }
    },
    app '-test' {
       icon 'icon/bengine-test-perf.ico',
-      link_project {
-         'testing',
-         'core',
-         'core-id'
-      }
+      link_project 'testing'
    }
 }
